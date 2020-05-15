6567e4cb1f8b3e0fc85c18c02a31e171a3340dfd
aliensmart <kaoua17@gmail.com>
2020-05-15 05:38:31 -0400

    fixed login problem

--- simplesocial/accounts/models.py
+++ simplesocial/accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib import auth
-
+from django.db import models
+from django.utils import timezone
 # Create your models here.
 
 class User(auth.models.User, auth.models.PermissionsMixin):
